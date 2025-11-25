4d84940165661567f6d71fd2b549deb51f26793b
KuwarGautam-01 <kuwargautam01@gmail.com>
2025-11-25 11:36:38 +0530

    Solved new DSA question

---
+++ Arrays/MaxAndMinElementInArray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        // code Here
+        int min = arr[0];
+        int max = arr[0];
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=1 ; i<arr.length ; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            } else if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        list.add(min);
+        list.add(max);
+        
+        return list;
+    }
+}
